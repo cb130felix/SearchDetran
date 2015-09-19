6e19026653386166f25b28975724ea287a6e7a49
GutoL <guto-acmilan@hotmail.com>
2015-09-18 21:05:06 -0300

    OK

--- SearchDetran/src/searchdetran/Cliente.java
+++ SearchDetran/src/searchdetran/Cliente.java
@@ -5,6 +5,10 @@
  */
 package searchdetran;
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+
 /**
  *
  * @author Guto L
@@ -13,6 +17,52 @@ public class Cliente {
     
     public static void main(String [] args){
         
+        boolean chek = false;
+        boolean trocarIP = false;
+        boolean enviar = true;
+        
+        while(enviar){
+            try {
+             
+            Socket conexao;
+            
+            // AQUI EU TENHO QUE CHAMAR O MÉTODO QUE DESCOBRE QUEM É SERVIDOR
+            
+        
+            } catch (Exception e) {
+            
+            
+            }
+        }//fim do while
+        
+       
+       
+    
+    }
+    
+    
+    public void descobreServidor(){
+    
+        int porta = 5000;
+        String broadcast = "255.255.255.255";
+        
+                
+                
+        try {
+            
+            InetAddress endereco = InetAddress.getByName(broadcast);
+            DatagramSocket requisicao = new DatagramSocket(porta,endereco);
+            
+            String pergunta = "quem eh servidor?";
+            byte menssagem[] = pergunta.getBytes();
+            
+            
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            
+        }
         
         
     
